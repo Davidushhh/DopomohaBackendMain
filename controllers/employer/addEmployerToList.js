e1eb40cb07bdf933881f2edf71cf547191676b0f
Nick Leso <nickleso.work@gmail.com>
2023-12-12 12:14:21 +0200

    optimize employer route

--- controllers/employer/addEmployerToList.js
+++ controllers/employer/addEmployerToList.js
@@ -1,9 +1,8 @@
 const { uid } = require("uid");
-const { pool } = require("../../models");
+const { pool, mailer } = require("../../models");
 
 const addEmployerToList = async (req, res, next) => {
   const { id } = req.params;
-
   const {
     company_name = null,
     contact_person = null,
@@ -12,12 +11,22 @@ const addEmployerToList = async (req, res, next) => {
     company_logo = null,
   } = req.body;
 
+  const employer_status = 0;
+
   try {
-    const addEmployerQuery = `INSERT INTO employers_list (employerId, company_name, contact_person, contact_phone, company_mail, company_logo, activation_key) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+    const upsertUserAndEmployerQuery = `
+    START TRANSACTION;
+    UPDATE dc_users SET employer_status = ? WHERE id = ?;
+    INSERT INTO employers_list (employerId, company_name, contact_person, contact_phone, company_mail, company_logo, activation_key)
+    VALUES (?, ?, ?, ?, ?, ?, ?);
+    COMMIT;
+  `;
 
     pool.query(
-      addEmployerQuery,
+      upsertUserAndEmployerQuery,
       [
+        employer_status,
+        id,
         id,
         company_name,
         contact_person,
@@ -42,7 +51,22 @@ const addEmployerToList = async (req, res, next) => {
           });
         }
 
-        return res.status(200).json({
+        await mailer.sendMail({
+          from: "info@dopomoha.carpathia.gov.ua",
+          to: "nickleso.work@gmail.com, trant755@gmail.com",
+          subject: `Новий роботодавець: ${company_name}`,
+          html: `<h2>Запит на отримання статусу роботодавця</h2>
+
+                <p>Копманія: ${company_name}</p>
+                <p>Контактна особа: ${contact_person}</p>
+                <p>Контактний номер телефону: ${contact_phone}</p>
+                <p>Контактний e-mail: ${company_mail}</p>
+
+                <p>Надати статус роботодавця можна за посиланням нижче:</p>
+                <a href="#">Перейти для наданя статус</a>`,
+        });
+
+        res.status(200).json({
           message: "user status changed and added to employers list",
           code: 201,
         });
--- routes/api/employerRouter.js
+++ routes/api/employerRouter.js
@@ -2,17 +2,9 @@ const express = require("express");
 const employersRouter = express.Router();
 
 const { ctrlWrapper, isEmployerCheck } = require("../../middlewares");
-const {
-  addEmployerToList,
-  changeEmployerStatus,
-} = require("../../controllers/employer");
+const { addEmployerToList } = require("../../controllers/employer");
 
 // зміна статусу юзера на роботодавця та його додавання в список роботодавців
-employersRouter.post(
-  "/:id",
-  isEmployerCheck,
-  ctrlWrapper(changeEmployerStatus),
-  ctrlWrapper(addEmployerToList)
-);
+employersRouter.post("/:id", isEmployerCheck, ctrlWrapper(addEmployerToList));
 
 module.exports = employersRouter;
